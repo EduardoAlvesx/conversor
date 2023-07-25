73ca2ce081f300e78df5ae643483616fc1ccaf51
Eduardo Alves <eduardostr12@gmail.com>
2023-07-25 17:27:28 -0300

    Classe Inicializador agora se chama Main

--- conversor-de-moedas/src/br/com/eduardoalvesx/conversor/modelo/Main.java
+++ conversor-de-moedas/src/br/com/eduardoalvesx/conversor/modelo/Main.java
@@ -1,7 +1,7 @@
 package br.com.eduardoalvesx.conversor.modelo;
 
 
-public class Inicializador {
+public class Main {
     public static void main(String[] args) {
        Estrutura estrutura = new Estrutura();
        estrutura.setVisible(true);
