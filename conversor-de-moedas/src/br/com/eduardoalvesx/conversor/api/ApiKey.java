dfde1f5e5e77b2a74a2f7841d87766110866b2e5
Eduardo Alves <eduardostr12@gmail.com>
2024-02-29 01:51:07 -0300

    removed class conversors

--- conversor-de-moedas/src/br/com/eduardoalvesx/conversor/api/ApiKey.java
+++ conversor-de-moedas/src/br/com/eduardoalvesx/conversor/api/ApiKey.java
@@ -3,7 +3,7 @@ package br.com.eduardoalvesx.conversor.api;
 import java.util.ResourceBundle;
 
 public class ApiKey {
-    private static final String key = ResourceBundle.getBundle("currencyApi").getString("key");;
+    private static final String key = ResourceBundle.getBundle("currencyApi").getString("key");
     public static String getKey() {
         return key;
     }
--- conversor-de-moedas/src/br/com/eduardoalvesx/conversor/models/Conversors.java
+++
@@ -1,6 +0,0 @@
-package br.com.eduardoalvesx.conversor.models;
-
-import java.util.List;
-
-public interface Conversors extends List<Conversor> {
-}
